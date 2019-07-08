b783e8d099e6e8a0709eefc50918dc96db4ac70a
zrh <2864700929@qq.com>
2019-07-08 22:06:45 +0800

    配置路由对象完成

--- src/components/AppSide.vue
+++ src/components/AppSide.vue
@@ -1,10 +1,9 @@
 <template>
      <div class="col-sm-3 col-md-2 sidebar">
         <ul class="nav nav-sidebar">
-          <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
-          <li><a href="#">Reports</a></li>
-          <li><a href="#">Analytics</a></li>
-          <li><a href="#">Export</a></li>
+          <!-- <li class="active"><a href="#"> 英雄列表</a></li>
+          <li><a href="#">武器列表</a></li>
+          <li><a href="#">装备列表</a></li> -->
         </ul>
       </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+
+Vue.config.productionTip = false
+
 //导入样式
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import './assets/css/index.css'
-Vue.config.productionTip = false
-
+//导入路由模块不需要写路径
+import  VueRouter from 'vue-router'
+//需要用use配置一下
+Vue.use(VueRouter)
+//创建路由对象
+const router = new VueRouter({
+  //配置路由规则
+  roures:[
+    // {name:'hero',path:'/hero',component:hero}
+  ]
+})
 new Vue({
   render: h => h(App),
+  //配置路由对象
+  router  
 }).$mount('#app')
