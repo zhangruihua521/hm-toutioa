c6ff6ea70772fd0778c3e1d8cc0f1e2f013bbc08
zrh <2864700929@qq.com>
2019-07-08 21:34:42 +0800

    实现头部组件

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+//导入样式
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import './assets/css/index.css'
 Vue.config.productionTip = false
 
 new Vue({
